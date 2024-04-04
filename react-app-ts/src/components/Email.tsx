e95c7506723f11dab0f3ef3d69e9776db32d70f6
zt <1808602879@qq.com>
2024-04-04 19:22:20 +0800

    嵌套路由和使用Hook路由 useRoutes

---
+++ react-app-ts/src/components/Email.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Email() {
+    return (
+        <div>
+            邮箱: 123874e34@qq.com
+        </div>
+    );
+}
+
+export default Email;
--- react-app-ts/src/index.tsx
+++ react-app-ts/src/index.tsx
@@ -3,17 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter } from "react-router-dom";
+import { HashRouter, BrowserRouter } from "react-router-dom";
 import 'bootstrap'
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <HashRouter>
+  <BrowserRouter>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </HashRouter>
+  </BrowserRouter>
   
 );
 
